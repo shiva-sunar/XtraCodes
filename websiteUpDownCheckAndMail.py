92469384e16b1239d93f2afd6775512009ec528e
Shiva Sunar <thinkshiva7@users.noreply.github.com>
2017-07-29 23:31:06 +0530

    Update and rename websiteUpDownChecker.py to websiteUpDownCheckAndMail.py

--- websiteUpDownCheckAndMail.py
+++ websiteUpDownCheckAndMail.py
@@ -1,3 +1,7 @@
+#Sometime system admin have to check if some websites are up or not and mail to their boss...
+#This code can automate it.
+#Tips: Make an Executable from this code with PyInstaller or whatever software you like 
+#And use Task Scheduler in Windows to Schedule this.
 import urllib;
 import time;
 import smtplib;
@@ -31,8 +35,8 @@ def messageFor(url):
 
 def sendEmail(emailAddress, message):
     emsg = MIMEMultipart();
-    emsg['From'] = "shiva.sunar@aksitservices.co.in";
-    emsg['To'] = "shiva.sunar@aksitservices.co.in";
+    emsg['From'] = "shiva.sunar@aksitservices.co.in"; #Change to your Requirement
+    emsg['To'] = "shiva.sunar@aksitservices.co.in";#Change to your Requirement
     emsg['Subject'] = "Website Status of " + getDate();
     emsg['Cc'] = "";
     body = "Dear Sir,\n" \
@@ -45,14 +49,15 @@ def sendEmail(emailAddress, message):
 
     try:
         print "Initiating Email Setup...";
-        server = smtplib.SMTP('smtp.mail.yahoo.com', 25);
+        server = smtplib.SMTP('smtp.mail.yahoo.com', 25);#Change to your Requirement
         print "Starting TLS Session to Mail Server...";
         server.starttls()
         print "Logging User to Mail Server..."
-        server.login(user="shiva.sunar@aksitservices.co.in",
-                     password="Tm9maGNobWFhQDc=".decode('base64'));
+        server.login(user="shiva.sunar@aksitservices.co.in",#Change to your Requirement
+                     #I encoded and decoded in base64 that if some person looks at code he don't know password immediately...
+                     password="PutYourBase64EncodedPasswordHere".decode('base64'));
         print "Sending Email...";
-        server.sendmail(from_addr="shiva.sunar@aksitservices.co.in",
+        server.sendmail(from_addr="shiva.sunar@aksitservices.co.in",#Change to your Requirement
                         to_addrs="shiva.sunar@aksitservices.co.in",  # emailAddress to send
                         msg=emsg.as_string())
         print "Email Sent...";
@@ -69,10 +74,10 @@ def sendEmail(emailAddress, message):
 def sendMessage(message):
     if ("DOWN" in message):
         # if there any site is down send mail to yourself to further check
-        sendEmail("yourself@aksitservices.co.in", message);
+        sendEmail("yourself@aksitservices.co.in", message);#Change to your Requirement
     else:
         # if all site are UP send OK Mail to your senior.
-        sendEmail("yourboss@aksitservices.co.in", message);
+        sendEmail("yourboss@aksitservices.co.in", message);#Change to your Requirement
     return;
 
 
@@ -83,9 +88,9 @@ def main():
         if (isUP("https://www.google.com")):
             print "Network is UP...";
             message = "";
-            message += messageFor("http://aksitservices.co.in") + "\n";
-            message += messageFor("https://www.akashmemorial.org") + "\n";
-            message += messageFor("http://www.akashsaxena.com") + "\n";
+            message += messageFor("http://aksitservices.co.in") + "\n";#Change to your Requirement
+            message += messageFor("https://www.akashmemorial.org") + "\n";#Change to your Requirement
+            message += messageFor("http://www.akashsaxena.com") + "\n";#Change to your Requirement
             sendMessage(message);
             break;
         else:
